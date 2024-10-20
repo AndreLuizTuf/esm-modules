8ff7077c196c7275e0a833e21bc084c1d60f1256
André Luiz <andre.luiz.a.f@hotmail.com>
2024-10-20 13:36:04 -0300

    Multiple Exports

--- src/utils/database.js
+++ src/utils/database.js
@@ -1,6 +1,16 @@
 async function connectToDatabase(dataName) {
-    // lógica de conexão
-    console.log(`conectado ao Banco ${dataName}`);    
+  // lógica de conexão
+  console.log(`conectado ao Banco ${dataName}`);
 }
 
-export default connectToDatabase;
+async function disconnectDatabase() {
+  // lógica de desconexão
+  console.log("desconectado do Banco de dados");
+}
+
+export default {connectToDatabase, disconnectDatabase};
+
+// module.exports = {
+//   connectToDatabase,
+//   disconnectDatabase,
+// };
